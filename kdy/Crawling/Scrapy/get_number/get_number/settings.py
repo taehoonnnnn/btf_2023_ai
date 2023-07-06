bfbe07586cfc1b30b2e414be48e7d82c4edeceaf
opeerats <opeerats@gamil.com>
2023-07-06 14:22:38 +0900

    Modify starter.py

--- kdy/Crawling/Scrapy/get_number/get_number/settings.py
+++ kdy/Crawling/Scrapy/get_number/get_number/settings.py
@@ -17,15 +17,15 @@ NEWSPIDER_MODULE = "get_number.spiders"
 #USER_AGENT = "get_number (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
