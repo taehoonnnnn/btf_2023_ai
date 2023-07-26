b701967a7a23e57a6d4966770bda4089725794db
opeerats <opeerats@gamil.com>
2023-07-26 16:07:33 +0900

    Modify score_eng cal_score_mean

--- kdy/summary/model/score/eng/keyword_eng.py
+++ kdy/summary/model/score/eng/keyword_eng.py
@@ -6,7 +6,7 @@ def compute_score(response, keywords):
 
 def main():
     # Load the data
-    data = pd.read_csv('output_TSRL.csv')
+    data = pd.read_csv('output_Abstractive.csv')
 
     # Apply the function to each row of the DataFrame
     score_keyword = data.apply(lambda row: compute_score(row['response'], 
